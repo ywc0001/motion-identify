752fb07d8099cc814e938507ff74f8768226a186
xingguangwei <13975280+xingguangwei123@user.noreply.gitee.com>
2024-01-16 03:59:34 +0000

    model3
    Signed-off-by: xingguangwei <13975280+xingguangwei123@user.noreply.gitee.com>

---
+++ Dumbbell detection/model3/model3.py
@@ -0,0 +1,114 @@
+# Importing the libraries
+import cv2
+import numpy as np
+import mediapipe as mp
+
+
+def calc_angle(a, b, c):  # 3D points
+    ''' Arguments:
+        a,b,c -- Values (x,y,z, visibility) of the three points a, b and c which will be used to calculate the
+                vectors ab and bc where 'b' will be 'elbow', 'a' will be shoulder and 'c' will be wrist.
+
+        Returns:
+        theta : Angle in degress between the lines joined by coordinates (a,b) and (b,c)
+    '''
+    a = np.array([a.x, a.y])  # , a.z])    # Reduce 3D point to 2D
+    b = np.array([b.x, b.y])  # , b.z])    # Reduce 3D point to 2D
+    c = np.array([c.x, c.y])  # , c.z])    # Reduce 3D point to 2D
+
+    ab = np.subtract(a, b)
+    bc = np.subtract(b, c)
+
+    theta = np.arccos(np.dot(ab, bc) / np.multiply(np.linalg.norm(ab), np.linalg.norm(
+        bc)))  # A.B = |A||B|cos(x) where x is the angle b/w A and B
+    theta = 180 - 180 * theta / 3.14  # Convert radians to degrees
+    return np.round(theta, 2)
+
+
+def infer():
+    mp_drawing = mp.solutions.drawing_utils  # Connecting Keypoints Visuals
+    mp_pose = mp.solutions.pose  # Keypoint detection model
+    left_flag = None  # Flag which stores hand position(Either UP or DOWN)
+    left_count = 0  # Storage for count of bicep curls
+    right_flag = None
+    right_count = 0
+
+    cap = cv2.VideoCapture(0)
+    pose = mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.5)  # Lnadmark detection model instance
+    while cap.isOpened():
+        _, frame = cap.read()
+
+        # BGR to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert BGR frame to RGB
+        image.flags.writeable = False
+
+        # Make Detections
+        results = pose.process(image)  # Get landmarks of the object in frame from the model
+
+        # Back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert RGB back to BGR
+
+        try:
+            # Extract Landmarks
+            landmarks = results.pose_landmarks.landmark
+            left_shoulder = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER]
+            left_elbow = landmarks[mp_pose.PoseLandmark.LEFT_ELBOW]
+            left_wrist = landmarks[mp_pose.PoseLandmark.LEFT_WRIST]
+            right_shoulder = landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+            right_elbow = landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW]
+            right_wrist = landmarks[mp_pose.PoseLandmark.RIGHT_WRIST]
+
+            # Calculate angle
+            left_angle = calc_angle(left_shoulder, left_elbow, left_wrist)  # Get angle
+            right_angle = calc_angle(right_shoulder, right_elbow, right_wrist)
+
+            # Visualize angle
+            cv2.putText(image, \
+                        str(left_angle), \
+                        tuple(np.multiply([left_elbow.x, left_elbow.y], [640, 480]).astype(int)), \
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, \
+                        str(right_angle), \
+                        tuple(np.multiply([right_elbow.x, right_elbow.y], [640, 480]).astype(int)), \
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Counter
+            if left_angle > 160:
+                left_flag = 'down'
+            if left_angle < 50 and left_flag == 'down':
+                left_count += 1
+                left_flag = 'up'
+
+            if right_angle > 160:
+                right_flag = 'down'
+            if right_angle < 50 and right_flag == 'down':
+                right_count += 1
+                right_flag = 'up'
+
+        except:
+            pass
+
+        # Setup Status Box
+        cv2.rectangle(image, (0, 0), (1024, 73), (10, 10, 10), -1)
+        cv2.putText(image, 'Left=' + str(left_count) + '    Right=' + str(right_count),
+                    (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Render Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv2.imshow('MediaPipe feed', image)
+
+        k = cv2.waitKey(30) & 0xff  # Esc for quiting the app
+        if k == 27:
+            break
+        elif k == ord('r'):  # Reset the counter on pressing 'r' on the Keyboard
+            left_count = 0
+            right_count = 0
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    infer()
